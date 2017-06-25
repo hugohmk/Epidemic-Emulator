07dff14f78a73b2bf559bcd25c3f58f448333eac
hugohmk <hugohmkling@gmail.com>
2017-06-25 09:41:51 -0300

    Added emulation scrip linux support

--- emulation.py
+++ emulation.py
@@ -1,13 +1,23 @@
 from subprocess import call
 import platform
+import argparse
 import os
+
 
 if __name__=="__main__":
     dir_path = os.path.dirname(os.path.realpath(__file__))
     dir_path_unix = dir_path.replace("\\","/")
-    if (platform.system()!="Windows"): dir_path = dir_path_unix
+    windows_plat = True
+    if (platform.system()!="Windows"):
+        windows_plat = False
+        dir_path = dir_path_unix
     cmd=""
-    
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-py","--python",default="python",
+                        help="Pyhton location (use this if python can not be located in PATH)")
+
+    args = parser.parse_args()
     with open(dir_path_unix+"/network.txt","r") as f:
         b = ""
         inter = "-i 1"
@@ -15,11 +25,17 @@ if __name__=="__main__":
             i = i.rstrip("\n").split("|")
             if len(i)<4:
                 break
-            cmd=cmd+"start {0}D:\\Python27\\python.exe {1} -id {2} {3}\n".format(b, dir_path+"\\main.py", i[0], inter)
-            b = "/B "
-            inter = ""
-
-    with open(dir_path_unix+"/emulation.bat","w") as f:
+            if windows_plat:
+                cmd = cmd + "start {0}{1} {2} -id {3} {4}\n".format(b, args.python, dir_path+"\\main.py", i[0], inter)
+                b = "/B "
+                inter = ""
+            else:
+                cmd = cmd + "{1} {2} -id {3} {4} {0}\n".format(b, args.python, dir_path+"/main.py", i[0], inter)
+                b = "&"
+                inter = ""
+                
+    extension = ".bat"
+    if not windows_plat: extension = ".sh"
+    
+    with open(dir_path_unix+"/emulation"+extension,"w") as f:
         f.write(cmd)
-
-    call(dir_path_unix+"/emulation.bat")
